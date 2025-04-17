9a9b2f7a1dfaf452d85cf359a3d34fd7a0fb3ce8
Khaled Zeffouni <83452303+khaled436@users.noreply.github.com>
2025-04-17 20:50:28 +0200

    127
    setting up ngSubmit

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
 import {ButtonComponent} from "../../../shared/button/button.component";
 import {ControlComponent} from "../../../shared/control/control.component";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
   imports: [
     ButtonComponent,
-    ControlComponent
+    ControlComponent,
+    FormsModule
   ],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
+
+  onSubmit(){
+  }
 
 }
