439c46059b1c911c1c3519b3927a8e4d41eb7026
Khaled Zeffouni <83452303+khaled436@users.noreply.github.com>
2025-05-16 12:08:59 +0200

    138
    seting up ticket model

--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {NewTicketComponent} from "./new-ticket/new-ticket.component";
+import {Ticket} from "./ticket.model";
 
 @Component({
   selector: 'app-tickets',
@@ -11,5 +12,7 @@ import {NewTicketComponent} from "./new-ticket/new-ticket.component";
   styleUrl: './tickets.component.css'
 })
 export class TicketsComponent {
+
+  tickets: Ticket[] = []
 
 }
